09575ab820d6add48752e252081081187cbb5b4f
ashish5-igtm <ashishpala456@gmail.com>
2026-04-28 10:16:43 +0530

    Delete backend directory

--- backend/src/main/java/com/assessment/monitor/repository/ChartDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assessment.monitor.repository;
-
-import com.assessment.monitor.model.ChartData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChartDataRepository extends JpaRepository<ChartData, Long> {
-}
